4ade0218e66a13d126a77ad755ca2ce4615bf19b
Pedro Suzuki <Pedro_Suzuki@epam.com>
2024-05-13 17:26:47 +0200

    Initial commit

---
+++ src/test/java/com/epam/course/CourseApplicationTest.java
@@ -0,0 +1,11 @@
+package com.epam.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
